2202fe5d5e54f563a0a1aa99301fa9955d4f89a3
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-28 17:06:45 +0100

    add: create logging utils functios

---
+++ src/utils/logging.js
@@ -0,0 +1,54 @@
+/**
+ * Combine all the logging options into one file.
+ *
+ * For now we log to the console, but if in the future we want to add a logging service
+ * we only need to adjust this file.
+ */
+
+/**
+ * logInfo should be used to log anything that can be used for debugging but is not a problem
+ */
+export const logInfo = (message) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log(message); // Only log in non-production environments
+  }
+};
+
+/**
+ * logWarning should be used to log anything that signals a problem that is not app breaking
+ */
+export const logWarning = (message) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.warn(message); // Only log in non-production environments
+  }
+};
+
+/**
+ * logError should be used to log anything that is app breaking
+ */
+export const logError = (error) => {
+  if (process.env.NODE_ENV !== "production") {
+    if (error instanceof Error) {
+      console.error(error.message, error.stack);
+      if (error.innerError) {
+        console.error("Inner Error: ", error.innerError);
+      }
+    } else if (typeof error === "object") {
+      console.error("ERROR: ", JSON.stringify(error, null, 2));
+    } else {
+      console.error("ERROR: ", error);
+    }
+  }
+};
+
+/**
+ * handleError should be used to log and handle any errors in the application
+ */
+export const handleError = (error) => {
+  // Log the error
+  logError(error);
+
+  // Handle the error as needed
+  // For example, you could send it to an error tracking service
+  // or display a user-friendly error message to the user
+};
