de6d021d4c94beb571ff38e2cea06d72d06590d1
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-28 16:28:34 +0100

    add: eslint.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 2000,
+  viewportHeight: 800,
+  viewportWidth: 1200,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000/",
   },
 });
---
+++ esling.config.js
@@ -0,0 +1,68 @@
+const { defineConfig } = require("eslint-define-config");
+const eslintPluginReact = require("eslint-plugin-react");
+const eslintPluginJsxA11y = require("eslint-plugin-jsx-a11y");
+const eslintPluginReactHooks = require("eslint-plugin-react-hooks");
+const eslintPluginCypress = require("eslint-plugin-cypress");
+
+module.exports = defineConfig([
+  {
+    ignores: ["**/.next/**"],
+    languageOptions: {
+      globals: {
+        "cypress/globals": true,
+        browser: true,
+        es2021: true,
+        jest: true,
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      "jsx-a11y": eslintPluginJsxA11y,
+      "react-hooks": eslintPluginReactHooks,
+      cypress: eslintPluginCypress,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/prop-types": ["warn"],
+      "no-unused-vars": ["warn", { varsIgnorePattern: "^React$" }],
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    // Specific configuration for .cy.js files
+    files: ["**/*.cy.js", "cypress.config.js"],
+    languageOptions: {
+      globals: {
+        "cypress/globals": true,
+      },
+    },
+    plugins: {
+      cypress: eslintPluginCypress,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "cypress/no-assertion-after-screenshot": "off",
+    },
+  },
+  {
+    // Specific configuration for logging.js
+    files: ["src/utils/logging.js"],
+    rules: {
+      "no-console": "off",
+    },
+  },
+]);
