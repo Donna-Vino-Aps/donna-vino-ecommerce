0c5d71ac9507b82c0002037ad7f801454080103b
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-01-31 13:49:13 +0100

    feat: Correct react code - But still get status code 404 on submit

--- src/components/LoginFormTest/SignUpForm.js
+++ src/components/LoginFormTest/SignUpForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import useFetch from "../../hooks/api/useFetch.js";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import { useLanguage } from "../../context/LanguageContext";
 
 const SignUpForm = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const years = Array.from(
     { length: new Date().getFullYear() - 1920 + 1 },
     (_, i) => 1920 + i,
@@ -24,13 +29,43 @@ const SignUpForm = () => {
     "December",
   ];
 
+  const onReceived = (data) => {
+    if (data.success) {
+      logInfo("Signup successful:", data);
+    } else {
+      console.error("Signup failed:", data);
+    }
+  };
+
+  // Fetch API for login request
+  const { performFetch, isLoading, error } = useFetch(
+    "/auth/sign-up",
+    onReceived,
+  );
+
+  // Handle form submission
+  const handleSignUp = (e) => {
+    e.preventDefault(); // Prevent default form reload
+
+    performFetch({
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        dateOfBirth,
+      }),
+    });
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-semibold text-center text-gray-700 mb-4">
           Sign Up
         </h2>
-        <form>
+        <form onSubmit={handleSignUp} className="signup-form">
           <div className="mb-4">
             <label
               className="block text-sm font-medium text-gray-600"
@@ -40,7 +75,9 @@ const SignUpForm = () => {
             </label>
             <input
               id="name"
-              type="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none"
               placeholder="Enter your name"
             />
@@ -56,6 +93,8 @@ const SignUpForm = () => {
             <input
               id="email"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none"
               placeholder="Enter your email"
             />
@@ -71,6 +110,8 @@ const SignUpForm = () => {
             <input
               id="password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none"
               placeholder="Enter your password"
             />
@@ -86,6 +127,8 @@ const SignUpForm = () => {
             <input
               id="confirmPassword"
               type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none"
               placeholder="Confirm your password"
             />
@@ -101,6 +144,9 @@ const SignUpForm = () => {
             <div className="relative w-full">
               <DatePicker
                 id="dateofbirth"
+                selected={dateOfBirth}
+                onChange={(date) => setDateOfBirth(date)}
+                placeholderText="Date of Birth"
                 className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-300"
                 renderCustomHeader={({ date, changeYear, changeMonth }) => (
                   <div
@@ -110,7 +156,6 @@ const SignUpForm = () => {
                       justifyContent: "center",
                     }}
                   >
-                    {/* Removed the month change buttons */}
                     <select
                       value={date.getFullYear()}
                       onChange={({ target: { value } }) => changeYear(value)}
@@ -136,15 +181,28 @@ const SignUpForm = () => {
                     </select>
                   </div>
                 )}
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
               />
             </div>
           </div>
 
-          <button className="w-full bg-secondary-normal text-white py-2 rounded-lg hover:bg-secondary-hover_normal transition">
-            Sign Up
+          <button
+            type="submit"
+            className="w-full bg-secondary-normal text-white py-2 rounded-lg hover:bg-secondary-hover_normal transition"
+            disabled={isLoading}
+          >
+            {isLoading ? "Signing Up..." : "Sign Up"}
           </button>
+
+          {/* Error Message */}
+          {error && (
+            <p className="text-red-500 text-center mt-2">
+              {
+                typeof error === "object" && typeof error.message === "string" // Double check type and if it is a string
+                  ? error.message
+                  : error.toString() // Fallback to string conversion
+              }
+            </p>
+          )}
         </form>
       </div>
     </div>
--- src/config/environment.js
+++ src/config/environment.js
@@ -5,7 +5,8 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // If it is production, we take the production URL, otherwise we take the development URL
 export const baseApiUrl = isProduction
-  ? process.env.NEXT_PUBLIC_API_URL_PRODUCTION || "https://defaulturl.com"
+  ? process.env.NEXT_PUBLIC_API_URL_PRODUCTION ||
+    "https://backend-donna-vino-c5c2e1c03c18.herokuapp.com"
   : process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT || "http://localhost:3000";
 
 // Log the baseApiUrl for debugging purposes
