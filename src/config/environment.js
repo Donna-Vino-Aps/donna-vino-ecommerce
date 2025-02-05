46bb2b2aa132646dd2dcffb24d3ecdf255e3200d
lenin del rio <lenindelrionicaragua@gmail.com>
2025-02-05 19:45:58 +0100

    fix: change the url to make the request to heroku

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,8 +1,8 @@
-// import { API_URL_HEROKU } from "@env";
-import { API_URL_LOCAL } from "@env";
+import { API_URL_HEROKU } from "@env";
+// import { API_URL_LOCAL } from "@env";
 import { logInfo } from "@/utils/logging";
 
-// export const baseApiUrl = API_URL_HEROKU;
-export const baseApiUrl = API_URL_LOCAL;
+export const baseApiUrl = API_URL_HEROKU;
+// export const baseApiUrl = API_URL_LOCAL;
 
 logInfo(`Server url: ${baseApiUrl}`);
