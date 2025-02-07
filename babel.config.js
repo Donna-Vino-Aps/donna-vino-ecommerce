433e3a8a7a31c6d7b841df96f47d060ca2cb346e
lenin del rio <lenindelrionicaragua@gmail.com>
2025-02-07 09:08:23 +0100

    Optimized Babel config: Use next/babel and ignore react-icons

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [["module:react-native-dotenv"]],
+  presets: ["next/babel"],
+  ignore: ["node_modules/react-icons"],
 };
