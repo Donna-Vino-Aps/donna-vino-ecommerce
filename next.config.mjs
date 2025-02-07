7a1ad20eba6307807beb273b8ffa1ed1bc0a9322
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-02-07 10:40:14 +0100

    chore: init - Changing the config-files to allow faster loading

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
+    config.module.rules.push({
+      test: "/\.mjs$/",
+      include: "/node_modules/",
+      type: "javascript/auto",
+    });
     return config;
   },
 };
