5665fad300cb9150d8f089692ba72c7c6e278296
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-02-10 16:13:35 +0100

    style: minor adjustments of logo positioning

--- src/components/LogIn/LogInScreen.js
+++ src/components/LogIn/LogInScreen.js
@@ -8,11 +8,13 @@ const LoginScreen = () => {
   return (
     <div className="flex justify-center items-center overflow-hidden mt-16 md:mt-[7.5rem] px-4 mx-14 md:mx-8 h-[36.5rem] md:h-full md:min-h-screen items-stretch">
       <section className="flex flex-col items-center rounded-3xl md:rounded-none justify-center bg-white flex-1 md:w-full md:h-full max-w-[36.563rem] max-h-[34.375rem] md:w-[50%] md:h-auto p-6">
-        <img
-          src="/images/donna-vino-logo-transparent.png"
-          alt="Donna Vino logo"
-          className="w-[6.25rem] mt-16"
-        />
+        <figure className="relative top-2 md:top-4">
+          <img
+            src="/images/donna-vino-logo-transparent.png"
+            alt="Donna Vino logo"
+            className="w-[7.25rem] mt-14"
+          />
+        </figure>
         <LoginForm />
       </section>
       <figure className="hidden md:flex justify-center flex-1 w-full max-w-[36.563rem] max-h-[34.375rem] md:w-[50%] md:h-auto overflow-hidden">
