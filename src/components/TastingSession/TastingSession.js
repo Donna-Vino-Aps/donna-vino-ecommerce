9697f032750e1d03240a4e1521e904e43b98dc49
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-02-11 15:22:23 +0100

    fix: responsivity changes and addition of left/right-buttons

--- src/components/TastingSession/TastingSession.js
+++ src/components/TastingSession/TastingSession.js
@@ -1,34 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { useLanguage } from "@/context/LanguageContext";
 
 const TastingSession = () => {
   const { translations } = useLanguage();
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(1);
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrevious = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
+    );
+  };
+
   return (
-    <section className="flex flex-row bg-white">
-      <figure className="md:w-[50%]">
+    <section className="relative flex flex-row bg-white">
+      <figure className="hidden md:flex md:w-[50%]">
         <img src="/images/dv-tasting.png" />
       </figure>
-      <div className="flex flex-col justify-center align-center font-barlow font-regular mx-8 md:w-[50%]">
+      <div className="flex flex-col justify-center align-center items-center md:items-start font-barlow font-regular mx-8 md:w-[50%]">
         <div>
-          <p className="md:text-titleMedium xl:text-titleLarge text-primary-dark">
+          <p className="titleMedium md:text-titleMedium xl:text-titleLarge text-primary-dark">
             {translations["tasting.subheading"]}🍷✨
           </p>
         </div>
-        <h2 className="md:text-displaySmall xl:text-displayMedium text-tertiary1-dark my-2 mr-8">
+        <h2 className="text-headlineLarge text-center md:text-left md:text-displaySmall xl:text-displayMedium text-tertiary1-dark my-2 mr-8">
           {translations["tasting.heading"]}
         </h2>
-        <p className="md:text-bodyMedium xl:text-bodyLarge mt-3 md:mb-5 xl:mb-10 mr-10">
+        <p className="text-bodySmall md:text-bodyMedium xl:text-bodyLarge md:mt-3 mb-3 md:mb-5 xl:mb-10 mr-10">
           {translations["tasting.paragraph"]}
         </p>
         <Button
           text={translations["tasting.button"]}
-          icon="@/icons/calender-alt-1.svg"
+          icon="/icons/calender-alt-1.svg"
           variant="redFullText"
           ariaLabel="Book tasting session"
           testId="book-tasting-button"
         ></Button>
+        <div className="hidden md:flex absolute bottom-14 right-12">
+          <button
+            onClick={handlePrevious}
+            className="w-[2.625rem] h-[2.625rem] rounded-full flex items-center justify-center active:bg-primary-hover_normal mr-[8px]"
+            aria-label="Previous image"
+            data-testid="carousel-previous-button"
+          >
+            <img
+              src="/icons/Prev.svg"
+              alt="Previous arrow"
+              data-testid="icon-previous-arrow"
+              loading="lazy"
+              role="presentation"
+            ></img>
+          </button>
+          <button
+            onClick={handleNext}
+            className="w-[2.625rem] h-[2.625rem]  rounded-full flex items-center justify-center active:bg-primary-hover_normal ml-[8px]"
+            aria-label="Next image"
+            data-testid="carousel-next-button"
+          >
+            <img
+              src="/icons/Next.svg"
+              alt="Next arrow"
+              data-testid="icon-next-arrow"
+              loading="lazy"
+              role="presentation"
+            ></img>
+          </button>
+        </div>
       </div>
     </section>
   );
