125af5e82aa782975318ab7bb8aaf4868ad72bec
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-02-11 10:37:02 +0100

    feat: importing image and fixing hover-effects

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import React from "react";
 import SignUpScreen from "../components/SignUpScreen/SignUpScreen";
 import LoginScreen from "@/components/LogIn/LogInScreen";
+import WineCard from "@/components/Card/WineCard";
 
 export default function Home() {
   return (
@@ -10,7 +11,14 @@ export default function Home() {
         className="flex flex-col gap-4 w-full bg-primary-light"
         data-testid="main-heading"
       >
+        <WineCard
+          title="Primitivo Susumani"
+          imageUrl="/images/primitivo-susmani.jpg"
+          price="39.00"
+          reviewsCount="14"
+        />
         <LoginScreen />
+
         <SignUpScreen />
       </main>
     </div>
--- src/components/Card/WineCard.js
+++ src/components/Card/WineCard.js
@@ -6,20 +6,22 @@ const WineCard = ({ title, imageUrl, price, reviewsCount }) => {
   //   const { translations } = useLanguage();
 
   return (
-    <article className="w-[23.125rem] h-[32rem]">
-      <figure>
-        <img src={imageUrl} />
+    <article className="w-[23.125rem] h-[32rem] bg-white mt-12 ml-4 rounded-lg shadow-lg hover:opacity-[0.96] hover:shadow-xl transition-shadow duration-300">
+      <figure className="mt-4 mb-6 overflow-hidden rounded-t-lg">
+        <img src={imageUrl} alt={title} className="h-[24rem] " />
       </figure>
-      <div className="flex flex-row space-between font-barlow">
-        <div className="flex flex-column">
-          <h4 className="text-headlineMedium font-regular">{title}</h4>
-          <p className="text-labelXLarge font-semibold">${price}</p>
+      <div className="flex justify-between items-center font-barlow">
+        <div className="flex flex-col">
+          <h4 className="text-headlineMedium font-regular pl-2 pb-1">
+            {title}
+          </h4>
+          <p className="text-labelXLarge font-semibold pb-3 pl-2">${price}</p>
         </div>
-        <div className="inline">
-          <p className="text-labelXLarge font-semibold">
+        <div className="flex flex-row gap-2 relative bottom-6 mr-2">
+          <p className="text-labelXLarge font-semibold text-tertiary2-darker">
             {reviewsCount} reviews
           </p>
-          <img src="@/icons/star-fill.svg" />
+          <img src="/icons/star-fill.svg" />
         </div>
       </div>
     </article>
