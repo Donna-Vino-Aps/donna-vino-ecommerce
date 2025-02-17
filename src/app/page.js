c69a58025a4513cbe6aaaddc2099681c36290fb3
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2025-02-17 17:46:23 +0100

    Fix/ little import mistake

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import LoginScreen from "@/components/LogIn/LogInScreen";
 import TastingSession from "@/components/TastingSession/TastingSession";
 import WineCard from "@/components/Card/WineCard";
 import TopWinesSection from "@/components/Slider/TopWinesSection";
-
+import SignUpScreen from "@/components/SignUpScreen/SignUpScreen";
 export default function Home() {
   return (
     <div className="flex w-[100%]" data-testid="home-container">
