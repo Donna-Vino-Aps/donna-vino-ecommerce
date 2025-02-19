4a9ee981fa83f434ca897e23f03a10a14168f3f4
lenin del rio <lenindelrionicaragua@gmail.com>
2025-02-19 19:08:29 +0100

    feat: Implement Google Sign-In with NextAuth directly in the API route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,32 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../../auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      logInfo("User signed in", user);
+      return true;
+    },
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = user?.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+      return session;
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
