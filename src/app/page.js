4f35f0c8e6e454f8208877f6612ae1a37857591e
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-02-21 16:09:00 +0100

    fix: some fixes

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="flex w-[100%]" data-testid="home-container">
       <main
-        className="flex flex-col gap-4 w-full bg-primary-light"
+        className="flex flex-col gap-4 w-full bg-white"
         data-testid="main-heading"
       >
         <TastingSession />
