efa4d183dbdb2cc4aa86f64a44b19b066e0fc35d
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-04 10:38:45 +0100

    feat: create simple user-dropdown

--- src/components/NavBar/UserDropdown.js
+++ src/components/NavBar/UserDropdown.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
   return (
-    <div>
-      <img
-        src="/icons/user-alt.svg"
-        alt="Search icon"
-        className="cursor-pointer"
-      />
+    <div className="relative">
+      {/* User Icon Button */}
+      <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer">
+        <img src="/icons/user-alt.svg" alt="Search icon" />
+      </button>
+
+      {/* Dropdown menu */}
+      <div
+        className={`flex absolute right-0 rounded-lg bg-white shadow-lg md:min-w-[10rem] md:min-h-[12.75rem] md:text-tertiary2-hover-dark ${isOpen ? "block" : "hidden"}`}
+      >
+        <ul className="relative left-6">
+          <li className="flex gap-1 my-2 mt-6">
+            <img src="/icons/wine-glass-1.svg" alt="wine glass icon"></img>
+            <a>My wines</a>
+          </li>
+          <li className="flex gap-1 my-2">
+            <img src="/icons/wine-glass-1.svg" alt="wine glass icon"></img>
+            <a>Orders</a>
+          </li>
+          <li className="flex gap-1 my-2">
+            <img src="/icons/wine-glass-1.svg" alt="wine glass icon"></img>
+            <a>Account</a>
+          </li>
+          <li className="flex gap-1 my-2">
+            <img src="/icons/wine-glass-1.svg" alt="wine glass icon"></img>
+            <a>Settings</a>
+          </li>
+          <hr className="border-[0.5px]"></hr>
+          <li className="flex gap-1 my-2">
+            <img src="/icons/wine-glass-1.svg" alt="wine glass icon"></img>
+            <a>Log out</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
