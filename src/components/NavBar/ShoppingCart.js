46967c8a41095839369f0cb337cadd608110dbcd
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-04 14:51:22 +0100

    feat: fixing the responsivity of buttons (navlinks still not centered)

--- src/components/NavBar/ShoppingCart.js
+++ src/components/NavBar/ShoppingCart.js
@@ -4,9 +4,9 @@ const ShoppingCart = () => {
   return (
     <div>
       <img src="/icons/cart.svg" alt="Search icon" className="cursor-pointer" />
-      <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1">
+      {/* <span className="absolute -top-1 -right-1 bg-primary-normal text-white text-xs rounded-full px-1">
         0
-      </span>
+      </span> */}
     </div>
   );
 };
--- src/components/NavBar/UserDropdown.js
+++ src/components/NavBar/UserDropdown.js
@@ -8,14 +8,14 @@ const UserDropdown = () => {
       {/* User Icon Button */}
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="cursor-pointer hidden md:block"
+        className="cursor-pointer hidden lg:block"
       >
-        <img src="/icons/user-alt.svg" alt="Search icon" />
+        <img src="/icons/user-alt.svg" alt="User icon" />
       </button>
 
       {/* Dropdown menu */}
       <div
-        className={`flex absolute right-0 rounded-lg bg-white shadow-lg md:min-w-[10rem] md:min-h-[12.75rem] md:text-tertiary1-dark ${isOpen ? "block" : "hidden"}`}
+        className={`flex absolute right-0 rounded-xl bg-white shadow-lg md:min-w-[10rem] md:min-h-[12.75rem] md:text-tertiary1-dark ${isOpen ? "block" : "hidden"}`}
       >
         <ul className="relative left-6">
           <li className="flex gap-1 my-4 mt-6 text-bodyMedium">
