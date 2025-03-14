9aac1a31ec4979179be28d7e45889d4b38cfbf21
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-14 12:56:41 +0100

    feat: created a CalendarTest to test the CalendarItem-component. Put CalendarTest into the wines-page. Styling more or less complete

--- src/app/wines/page.js
+++ src/app/wines/page.js
@@ -1,11 +1,13 @@
 "use client";
 
+import CalendarTest from "@/components/Calendar/CalendarTest";
 import React from "react";
 
 const Wines = () => {
   return (
     <div>
       <h1 className="text-headlineLarge text-center">Wines</h1>
+      <CalendarTest />
     </div>
   );
 };
--- src/components/Calendar/CalendarItem.js
+++ src/components/Calendar/CalendarItem.js
@@ -7,43 +7,55 @@ const BASE_CALENDARITEM_CLASSES = `
    h-[7.938rem] w-[12.5rem] text-labelXLarge font-semibold rounded-tl-[16px] rounded-bl-[62.5px]
 `;
 
-const VARIANT_CLASSES = {
-  none: "bg-white",
-  today: "bg-primary-light_active",
-  open: "bg-[#34C759]",
-  limited: "bg-[#ff9500]",
-  full: "bg-[#FF3B30]",
-};
-
 const CalendarItem = ({
   dayOfMonth,
   icon,
-  seatsAvailable,
-  seatsTotal,
+  seatsAvailable = 0,
+  seatsTotal = 0,
   variant = "none",
   onClick,
 }) => {
   const isFull = seatsAvailable === 0;
   const percentageAvailable = (seatsAvailable / seatsTotal) * 100;
 
+  // Get today's date
+  const today = new Date();
+  const todayDayOfMonth = today.getDate(); // Get the day of the month (1-31)
+
+  // Check if today's day matches the dayOfMonth prop
+  const isToday = dayOfMonth === todayDayOfMonth;
+
+  let bgColor;
+  if (isFull) {
+    bgColor = "bg-[#FF3B30] text-tertiary1-light"; // Red if full
+  } else if (isToday) {
+    bgColor = "bg-primary-active text-tertiary1-light"; // light pink if today
+  } else if (percentageAvailable <= 30) {
+    bgColor = "bg-[#ff9500] text-tertiary1-light"; // Yellow if limited
+  } else if (seatsTotal === 0) {
+    bgColor = "bg-[#ffffff]"; // White if there is no event on this day
+  } else {
+    bgColor = "bg-[#34C759] text-tertiary1-light"; // Green if many seats available
+  }
+
   const calendarItemClass = `
     ${BASE_CALENDARITEM_CLASSES}
-    ${VARIANT_CLASSES[variant] || ""}
-  `.trim();
+    ${bgColor}
+      `.trim();
 
   return (
     <article
-      className={`relative h-[7.938rem] w-[12.5rem] bg-white ${isFull ? "hover:cursor-not-allowed" : "hover:cursor-pointer"}`}
+      className={`relative h-[7.938rem] w-[12.5rem] bg-white border-tertiary1-light border-[1px] ${isFull ? "hover:cursor-not-allowed" : "hover:cursor-pointer"}`}
       onClick={onClick}
     >
       <div
         className={`${calendarItemClass} 
         ${variant === "limited" && percentageAvailable > 50 ? "opacity-85" : "opacity-100"}`}
       >
-        <p className="absolute top-2 left-2">{dayOfMonth}</p>
-        <div className="absolute bottom-2 left-6">
+        <p className="absolute top-3 left-3">{dayOfMonth}</p>
+        <div className="absolute bottom-2 right-6">
           {icon && <img src={icon} alt="attendants icon" className="w-4 h-4" />}
-          <p>{`Seats: ${seatsAvailable}`}</p>
+          <p className="text-white">{`Seats: ${seatsAvailable}`}</p>
         </div>
       </div>
     </article>
