f4650f6478c0d741324f1d2a8265cd461adafdbf
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-18 09:51:14 +0100

    feat: Enabled MUI Datepicker when clicking the middle of DateSelector-component

--- src/components/Calendar/DateSelector.js
+++ src/components/Calendar/DateSelector.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import PickerWithButtonField from "./PickerWithButtonField";
 
 const DateSelector = () => {
+  const [value, setValue] = useState(null);
+  const [open, setOpen] = useState(false);
+
   return (
-    <section className="flex justify-center items-center mx-auto bg-primary-light w-full h-[4rem] max-w-[calc(7*12.5rem-2px)] mb-4 rounded-lg">
-      <div className="flex gap-2">
-        <p className="text-headlineSmall text-tertiary1-darker">Select date</p>
-        <img
-          src="/icons/calender-alt-1-gray.svg"
-          className="relative bottom-[2px]"
-        />
-      </div>
-    </section>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <section className="flex justify-center items-center mx-auto bg-primary-light w-full h-[4rem] max-w-[calc(7*12.5rem-2px)] mb-4 rounded-lg">
+        <div className="flex gap-2 hover:cursor-pointer">
+          <p
+            className="text-headlineSmall text-tertiary1-darker"
+            onClick={() => setOpen(true)}
+          >
+            Select date
+          </p>
+          <PickerWithButtonField
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+            open={open}
+            setOpen={setOpen}
+          />
+        </div>
+      </section>
+    </LocalizationProvider>
   );
 };
 
---
+++ src/components/Calendar/PickerWithButtonField.js
@@ -0,0 +1,112 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+function ButtonField(props) {
+  const {
+    setOpen,
+    id,
+    disabled,
+    InputProps: { ref } = {},
+    inputProps: { "aria-label": ariaLabel } = {},
+  } = props;
+
+  return (
+    <Button
+      variant="none"
+      id={id}
+      disabled={disabled}
+      ref={ref}
+      aria-label={ariaLabel}
+      onClick={() => setOpen?.((prev) => !prev)}
+      style={{
+        padding: 0,
+        minWidth: "auto",
+      }}
+    >
+      <img
+        src="/icons/calender-alt-1-gray.svg" // Path to your image
+        alt="Calendar Icon"
+        className="h-[36px] w-[33px]" // Adjust the size to fit as needed
+      />
+    </Button>
+  );
+}
+
+ButtonField.propTypes = {
+  setOpen: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  disabled: PropTypes.bool,
+  InputProps: PropTypes.shape({
+    ref: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  }),
+  inputProps: PropTypes.shape({
+    "aria-label": PropTypes.string,
+  }),
+};
+
+function ButtonDatePicker({
+  value,
+  onChange,
+  open,
+  onClose,
+  onOpen,
+  ...props
+}) {
+  return (
+    <DatePicker
+      value={value}
+      onChange={onChange}
+      slots={{ ...props.slots, field: ButtonField }}
+      slotProps={{ ...props.slotProps, field: { setOpen: onOpen } }}
+      {...props}
+      open={open}
+      onClose={onClose}
+      onOpen={onOpen}
+    />
+  );
+}
+
+ButtonDatePicker.propTypes = {
+  slots: PropTypes.object,
+  slotProps: PropTypes.object,
+  label: PropTypes.string,
+  value: PropTypes.object,
+  onChange: PropTypes.func,
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onOpen: PropTypes.func,
+};
+
+export default function PickerWithButtonField({
+  value,
+  onChange,
+  open,
+  setOpen,
+}) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ButtonDatePicker
+        label={value == null ? null : value.format("MM/DD/YYYY")}
+        value={value}
+        onChange={onChange}
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      />
+    </LocalizationProvider>
+  );
+}
+
+PickerWithButtonField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.object,
+  onChange: PropTypes.func.isRequired,
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onOpen: PropTypes.func,
+  setOpen: PropTypes.func.isRequired,
+};
