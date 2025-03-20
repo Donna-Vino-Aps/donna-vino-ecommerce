5f29839fce4288a6528ba60ed2692b655c6518f6
Natalia Lapina <lapinann08@gmail.com>
2025-03-20 14:11:12 +0100

    fix: remove unnecessary div

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -3,11 +3,7 @@ import SignUpScreen from "@/components/SignUpScreen/SignUpScreen";
 import React from "react";
 
 const SignUp = () => {
-  return (
-    <div>
-      <SignUpScreen />
-    </div>
-  );
+  return <SignUpScreen />;
 };
 
 export default SignUp;
