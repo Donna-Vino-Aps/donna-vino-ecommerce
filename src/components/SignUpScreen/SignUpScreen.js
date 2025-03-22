870194ddadc7964f8ee461b7d686fecf84b8bc9c
Natalia Lapina <lapinann08@gmail.com>
2025-03-22 15:31:31 +0100

    feat: update spinner

--- src/components/SignUpScreen/SignUpScreen.js
+++ src/components/SignUpScreen/SignUpScreen.js
@@ -349,7 +349,7 @@ const SignUpScreen = () => {
                 {/* Loading Indicator */}
                 {isLoading && (
                   <div className="flex justify-center items-center mt-4">
-                    <div className="w-8 h-8 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin" />
+                    <div className="w-8 h-8 border-t-transparent border-solid animate-spin rounded-full border-primary-normal border-2" />
                   </div>
                 )}
               </form>
