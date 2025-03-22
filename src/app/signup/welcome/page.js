c7a925fc4261c7887a2057f9b4c322b26c1bcc94
Natalia Lapina <lapinann08@gmail.com>
2025-03-22 13:23:19 +0100

    feat: add support two languages to welcome page

--- src/app/signup/welcome/page.js
+++ src/app/signup/welcome/page.js
@@ -2,14 +2,17 @@
 import React from "react";
 import Image from "next/image";
 import Button from "@/components/Button/Button";
+import { useLanguage } from "@/context/LanguageContext";
 import { useRouter } from "next/navigation";
 
 const Welcome = () => {
+  const { translations } = useLanguage();
   const router = useRouter();
+
   return (
     <section className="my-4 bg-primary-light sm:bg-dots-lg sm:bg-dots-size-lg bg-dots-sm bg-dots-size-sm">
       <div className="flex flex-col justify-center items-center sm:py-24 py-4 mx-2">
-        <div className="bg-tertiary2-light items-center justify-center rounded-2xl shadow-lg px-5 py-8 sm:px-20 sm:py-10 max-w-[35rem] w-full">
+        <div className="bg-tertiary2-light items-center justify-center rounded-2xl shadow-lg px-5 py-8 sm:px-16 sm:py-10 max-w-[35rem] w-full">
           <div className="flex justify-center mb-4">
             <Image
               src="/icons/message-check.svg"
@@ -22,29 +25,30 @@ const Welcome = () => {
             id="email-verification-title"
             className="text-titleLarge sm:text-headlineMedium text-center mb-6 sm:mb-4"
           >
-            👏 Done! Welcome on board! 👏
+            {translations["signUp.welcome.title"]}
           </h1>
 
-          <p className="text-bodyLarge mb-6 sm:mb-4 text-left">
-            Your email has been successfully registered in our system.
-            <strong> Please check your inbox</strong> for the confirmation email
-            regarding your registration. Once confirmed, you will be able to
-            access your personalized profile dashboard, where you can upload
-            your profile picture, update your information, and complete all the
-            required details to streamline your future orders and enhance your
-            shopping experience.
-          </p>
+          <p
+            className="text-bodyLarge mb-6 sm:mb-4 text-left"
+            dangerouslySetInnerHTML={{
+              __html: translations["signUp.welcome.message"],
+            }}
+          />
+
           <Button
-            text="Back to the log in page"
+            text={translations["signUp.welcome.button"]}
             variant="redWide"
             onClick={() => router.push("/login")}
             testId="login-button"
-            ariaLabel="Back to the log in page"
+            ariaLabel={translations["signUp.welcome.button"]}
           />
+
           <div className="text-left mt-6 sm:mt-8">
-            <span className="text-bodyLarge">Didn't receive the email? </span>
+            <span className="text-bodyLarge">
+              {translations["signUp.welcome.resend"]}{" "}
+            </span>
             <button className="font-semibold underline hover:text-primary-hover_normal focus:outline-none inline-block">
-              Resend
+              {translations["signUp.welcome.resend.button"]}
             </button>
           </div>
         </div>
