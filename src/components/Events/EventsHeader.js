fb2f90759f34d5d76ce87a9bcfa18b0640a5faf4
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-26 13:37:13 +0100

    style: adding dotted shape to the background

--- src/components/Events/EventsHeader.js
+++ src/components/Events/EventsHeader.js
@@ -2,11 +2,43 @@ import React from "react";
 
 const EventsHeader = () => {
   const infoArticle =
-    "flex flex-col gap-2 mx-4 max-w-[27.313rem] max-h-[19.563rem] bg-white text-tertiary1-darker space-y-3 p-4 my-12 mx-8 rounded-2xl border-[2px] border-primary-active";
+    "flex flex-col gap-2 max-w-[27.313rem] max-h-[19.563rem] bg-white text-tertiary1-darker space-y-3 p-4 my-12 px-4 mx-4 rounded-2xl border-[2px] border-primary-active";
 
   return (
-    <section className="flex flex-col bg-primary-light mb-8">
-      <section className="flex flex-col md:flex-row">
+    <section className="relative flex flex-col bg-primary-light mb-8 z-10 w-full overflow-hidden">
+      <div className="absolute bottom-0 left-0 sm:right-auto sm:left-0 sm:bottom-0 flex justify-start z-[-1]">
+        <div className="relative w-[4.5rem] h-[6.75rem] sm:w-[10.35rem] sm:h-[6.93rem] max-w-[50vw]">
+          <img
+            src="/design-elements/Dotted Shape.svg"
+            alt="dotted shape"
+            className="absolute bottom-0 left-0 sm:top-auto sm:bottom-0 sm:right-0 w-[3rem] h-[3rem] sm:w-[4.6rem] sm:h-[4.6rem]"
+            data-testid="dotted-shape-1"
+          />
+          <img
+            src="/design-elements/Dotted Shape.svg"
+            alt="dotted shape"
+            className="absolute bottom-[40%] left-[25%] sm:bottom-[262%] sm:left-[385%] w-[3rem] h-[3rem] sm:w-[4.6rem] sm:h-[4.6rem]"
+            data-testid="dotted-shape-2"
+          />
+        </div>
+      </div>
+      <div className="absolute bottom-0 right-0 sm:right-auto sm:left-[61.5%] sm:bottom-0 flex justify-start z-[-1]">
+        <div className="relative w-[4.5rem] h-[6.75rem] sm:w-[10.35rem] sm:h-[6.93rem] max-w-[50vw]">
+          <img
+            src="/design-elements/Dotted Shape.svg"
+            alt="dotted shape"
+            className="absolute bottom-0 left-0 sm:top-auto sm:bottom-0 sm:left-0 w-[3rem] h-[3rem] sm:w-[4.6rem] sm:h-[4.6rem]"
+            data-testid="dotted-shape-3"
+          />
+          <img
+            src="/design-elements/Dotted Shape.svg"
+            alt="dotted shape"
+            className="absolute bottom-0 right-0 sm:bottom-[262%] sm:left-[350%] w-[3rem] h-[3rem] sm:w-[4.6rem] sm:h-[4.6rem]"
+            data-testid="dotted-shape-4"
+          />
+        </div>
+      </div>
+      <section className="flex flex-col md:flex-row z-1">
         <div className="flex flex-col justify-center text-start mx-8 gap-6">
           <h1 className="text-displayMedium xl:max-w-[70%]">
             Secure your spot for our upcoming events.🍷✨
@@ -17,14 +49,14 @@ const EventsHeader = () => {
             expert team.
           </p>
         </div>
-        <figure className="">
+        <figure className="overflow-hidden">
           <img
             src="/images/events-header-unsplash.jpg"
-            className="max-w-[54.375rem] max-h-[29.313rem] md:rounded-tl-[11.531rem]"
+            className="min-w-[54.375rem] max-h-[29.313rem] md:rounded-tl-[11.531rem] max-w-full"
           />
         </figure>
       </section>
-      <section className="flex flex-row justify-center">
+      <section className="flex flex-row justify-center z-1">
         <article className={infoArticle}>
           <h3 className="text-headlineSmall">
             1. Check availability on the calendar
