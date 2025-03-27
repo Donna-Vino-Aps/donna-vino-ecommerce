437b874762d185bbd23c8b4283f041aa7519e9b5
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2025-03-27 13:04:10 +0100

    fix/ buttons

--- src/components/EventRegistrationModal/EventRegistrationModal.js
+++ src/components/EventRegistrationModal/EventRegistrationModal.js
@@ -189,7 +189,7 @@ function EventRegistrationModal({ isOpen, onClose, eventDetails = {} }) {
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
                   required
-                  className="w-[49%] border border-tertiary2-normal rounded p-2 text-bodyLarge"
+                  className="w-full md:w-[49%] border border-tertiary2-normal rounded p-2 text-bodyLarge"
                   placeholder="Phone Number*"
                 />
               </div>
@@ -254,17 +254,18 @@ function EventRegistrationModal({ isOpen, onClose, eventDetails = {} }) {
                 </label>
               </div>
               <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full">
-                <div className="flex w-full gap-2 mb-4 md:mb-0">
+                <div className="flex flex-col md:flex-row w-full gap-2 mb-4 md:mb-0">
                   <button
                     type="button"
-                    className="flex-1 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded font-medium"
+                    className="w-full md:flex-1 bg-primary-normal hover:bg-primary-hover_normal text-white px-4 py-2 rounded font-medium"
                     onClick={onClose}
                   >
                     Close
                   </button>
+
                   <button
                     type="submit"
-                    className="flex-1 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded font-medium disabled:bg-blue-500 disabled:hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
+                    className="w-full md:flex-1 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded font-medium disabled:bg-blue-500 disabled:hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
                     disabled={!agree || availableSeats === 0}
                   >
                     <span>Pay with</span>
