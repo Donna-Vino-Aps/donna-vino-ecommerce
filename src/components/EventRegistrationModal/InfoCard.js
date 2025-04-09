63cf48fd44ba534d64eed43d4b534bb4da8ca02a
Natalia Lapina <lapinann08@gmail.com>
2025-04-09 10:03:55 +0200

    refactor: simplify InfoCard component by removing unused props and integrating ShopifyRichTextRenderer

--- src/components/EventRegistrationModal/InfoCard.js
+++ src/components/EventRegistrationModal/InfoCard.js
@@ -1,44 +1,31 @@
 import React from "react";
 import Image from "next/image";
 import PropTypes from "prop-types";
+import ShopifyRichTextRenderer from "../common/ShopifyRichTextRenderer";
 
-function InfoCard({
-  title,
-  imageUrl,
-  imageAlt = "",
-  description,
-  winery,
-  wine,
-  bgClass,
-}) {
+function InfoCard({ title, imageUrl, imageAlt = "", description, bgClass }) {
   return (
     <div className={`${bgClass} shadow-md pb-4 rounded-[2rem]`}>
       {imageUrl && (
-        <div className="relative w-full h-64 mb-2">
+        <div className="relative w-full h-52 mb-8">
           <Image
             src={imageUrl}
             alt={imageAlt}
             fill
             sizes="(max-width: 768px) 100vw, 50vw"
             priority
-            className="object-cover rounded-[2rem]"
+            className="object-cover rounded-t-[2rem]"
           />
         </div>
       )}
 
-      <h3 className="text-displaySmall text-terniary1-darker font-medium font-barlow mb-6 pr-6 pl-6">
+      <h3 className="text-displaySmall text-terniary1-darker font-medium font-barlow mb-6 px-5">
         {title}
       </h3>
 
-      <p className="mb-2 pr-2 pl-6 text-bodyLarge">{description}</p>
-
-      {bulletPoints.length > 0 && (
-        <ul className="list-none space-y-1 pr-6 pl-6 text-bodyLarge">
-          {bulletPoints.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </ul>
-      )}
+      <div className="mb-8 px-5 text-bodyLarge">
+        <ShopifyRichTextRenderer jsonString={description} />
+      </div>
     </div>
   );
 }
@@ -49,8 +36,6 @@ InfoCard.propTypes = {
   bulletPoints: PropTypes.arrayOf(PropTypes.string),
   imageUrl: PropTypes.string,
   imageAlt: PropTypes.string,
-  winery: PropTypes.string,
-  wine: PropTypes.string,
   bgClass: PropTypes.string,
 };
 
