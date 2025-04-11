9f7fd5270ae0b83798494e2e6b86565bcd324ac5
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-04-11 21:45:09 +0200

    chore: import Suspense

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Button from "@/components/Button/Button";
 import { useLanguage } from "@/context/LanguageContext";
