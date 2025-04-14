a757360462d691c8ed8b99d37a5725fa77399bc0
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-04-14 18:50:24 +0200

    Delete redundant li

--- src/components/NavBar/UserDropdown/Variants/Separator.js
+++ src/components/NavBar/UserDropdown/Variants/Separator.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function Separator() {
   return (
-    // <li className="flex gap-1 text-bodyMedium">
-    <hr className="border-[0.5px] min-w-[7.5rem] border-tertiary1-active"></hr>
-    // </li>
+    <hr className="border-[0.5px] min-w-[7.5rem] border-tertiary1-active my-3"></hr>
   );
 }
