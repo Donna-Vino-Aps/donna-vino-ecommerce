7e9252efe8ac5d99991d7de945d6d4803e9bca75
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-04-14 10:31:11 +0200

    fix: slight fixes of the colorinfo-component width

--- src/components/Calendar/ColorInfo.js
+++ src/components/Calendar/ColorInfo.js
@@ -17,7 +17,7 @@ const ColorInfo = () => {
   //   return () => window.removeEventListener("resize", handleResize);
   // }, []);
   return (
-    <div className="flex justify-center items-center rounded-[0.5rem] bg-tertiary2-normal h-[88px] md:h-10 mt-3 w-full md:max-w-[calc(7*6.22rem)]">
+    <div className="flex justify-center items-center rounded-[0.5rem] bg-tertiary2-normal h-[88px] md:h-10 mt-3 w-full md:max-w-[calc(7*6.22rem-6px)] lg:md:max-w-[calc(7*6.22rem-2px)]">
       <div className="grid grid-cols-2 px-2 gap-6 justify-space-between md:gap-4 md:flex md:flex-row justify-center items-center">
         <article className="flex items-center gap-2">
           <div className="w-4 h-4 rounded-full bg-primary-active"></div>
