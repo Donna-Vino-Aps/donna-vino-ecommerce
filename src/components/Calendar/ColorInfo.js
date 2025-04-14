5f99603a3cdac2453419220626e35d72d476d7b8
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-04-14 09:43:59 +0200

    feat: making colorInfo not use images any more + rounding the top of the calendar (0.5rem rounding)

--- src/components/Calendar/ColorInfo.js
+++ src/components/Calendar/ColorInfo.js
@@ -1,28 +1,49 @@
 import React from "react";
+import { useLanguage } from "@/context/LanguageContext";
 
 const ColorInfo = () => {
-  const [isMobile, setIsMobile] = React.useState(
-    typeof window !== "undefined" && window.innerWidth < 768,
-  );
+  // const [isMobile, setIsMobile] = React.useState(
+  //   typeof window !== "undefined" && window.innerWidth < 768,
+  // );
 
-  React.useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+  const { translations } = useLanguage();
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  // React.useEffect(() => {
+  //   // const handleResize = () => {
+  //   //   setIsMobile(window.innerWidth < 768);
+  //   // };
+
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
   return (
-    <div className="flex justify-center items-center rounded-[0.5rem] bg-tertiary2-normal h-[88px] md:h-10 mt-3">
-      <img
-        src={
-          isMobile
-            ? "/icons/color-info-calendar-mobile.svg"
-            : "/icons/color-info-calendar.svg"
-        }
-        className="md:h-[1rem] w-max-[34rem] mx-8"
-      />
+    <div className="flex justify-center items-center rounded-[0.5rem] bg-tertiary2-normal h-[88px] md:h-10 mt-3 w-full md:max-w-[calc(7*6.22rem)]">
+      <div className="grid grid-cols-2 px-2 gap-6 justify-space-between md:gap-4 md:flex md:flex-row justify-center items-center">
+        <article className="flex items-center gap-2">
+          <div className="w-4 h-4 rounded-full bg-primary-active"></div>
+          <p className="text-labelMedium font-medium relative bottom-[1px]">
+            {translations["calendar.colorinfo.today"]}
+          </p>
+        </article>
+        <article className="flex items-center gap-2">
+          <div className="w-4 h-4 rounded-full bg-calendar-open"></div>
+          <p className="text-labelMedium font-medium relative bottom-[1px]">
+            {translations["calendar.colorinfo.available"]}
+          </p>
+        </article>
+        <article className="flex items-center gap-2">
+          <div className="w-4 h-4 rounded-full bg-calendar-limited"></div>
+          <p className="text-labelMedium font-medium relative bottom-[1px]">
+            {translations["calendar.colorinfo.limited"]}
+          </p>
+        </article>
+        <article className="flex items-center gap-2">
+          <div className="w-4 h-4 rounded-full bg-calendar-full"></div>
+          <p className="text-labelMedium font-medium relative bottom-[1px]">
+            {translations["calendar.colorinfo.full"]}
+          </p>
+        </article>
+      </div>
     </div>
   );
 };
