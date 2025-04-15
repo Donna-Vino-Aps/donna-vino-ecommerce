88c7768941a85222fff6cd28d42360fd7870c2b9
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-04-15 11:13:45 +0200

    Add styles

--- src/components/NavBar/UserDropdown/Variants/Link.js
+++ src/components/NavBar/UserDropdown/Variants/Link.js
@@ -4,16 +4,12 @@ import PropTypes from "prop-types";
 
 export default function MenuLink({ image, url, title, onClose }) {
   return (
-    <>
-      <img
-        src={image.src}
-        alt={image.alt}
-        className="relative bottom-[1px]"
-      ></img>
+    <div className="flex justify-center gap-2 hover:bg-primary-light rounded-md p-2 transition duration-200">
+      <img src={image.src} alt={image.alt} className="relative"></img>
       <Link href={url} onClick={onClose}>
         {title}
       </Link>
-    </>
+    </div>
   );
 }
 
@@ -21,4 +17,5 @@ MenuLink.propTypes = {
   image: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
 };
