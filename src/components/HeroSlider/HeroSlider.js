79671fbdb088357e7bf2f8f85fbf42cf5582ab8e
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-04-17 10:10:11 +0200

    fix: refactoring the variables into the slides-array and rewriting code to reflect this change

--- src/components/HeroSlider/HeroSlider.js
+++ src/components/HeroSlider/HeroSlider.js
@@ -7,48 +7,49 @@ const HeroSlider = () => {
   const { translations } = useLanguage();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // Storing the different texts and images used in the slider
-  const subheadings = [
-    "tasting.subheading2",
-    "tasting.subheading1", // video slide
-    "tasting.subheading3",
+  const slides = [
+    {
+      type: "video",
+      subheading: "tasting.subheading1",
+      heading: "tasting.heading1",
+      paragraph: "tasting.paragraph1",
+      buttonText: "tasting.button1",
+      buttonIcon: "/icons/envelope-alt.svg",
+      media:
+        "https://res.cloudinary.com/db3h63tns/video/upload/v1741092198/e-commerce-hero-section_egcjvf.mp4",
+      url: "/events",
+    },
+    {
+      type: "image",
+      subheading: "tasting.subheading2",
+      heading: "tasting.heading2",
+      paragraph: "tasting.paragraph2",
+      buttonText: "tasting.button2",
+      buttonIcon: "/icons/calender-alt-1.svg",
+      media: "/images/hero-wines.jpg",
+      url: "", // blank for now until the webshop is ready
+    },
+    {
+      type: "image",
+      subheading: "tasting.subheading3",
+      heading: "tasting.heading3",
+      paragraph: "tasting.paragraph3",
+      buttonText: "tasting.button3",
+      buttonIcon: "/icons/wine-glass-white.svg",
+      media: "/images/hero-newsletter.jpg",
+      url: "https://www.donnavino.dk/#subscribe",
+    },
   ];
-
-  const headings = ["tasting.heading2", "tasting.heading1", "tasting.heading3"]; // put these in numerical order for production
-
-  const paragraphs = [
-    "tasting.paragraph2",
-    "tasting.paragraph1", // video slide
-    "tasting.paragraph3",
-  ];
-
-  const buttons = ["tasting.button2", "tasting.button1", "tasting.button3"]; // put these in numerical order for production
-
-  const buttonIcons = [
-    "/icons/envelope-alt.svg",
-    "/icons/calender-alt-1.svg", // video slide
-    "/icons/wine-glass-white.svg",
-  ];
-
-  const images = [
-    "/images/hero-wines.jpg",
-    "https://res.cloudinary.com/db3h63tns/video/upload/v1741092198/e-commerce-hero-section_egcjvf.mp4", // video slide
-    "/images/hero-newsletter.jpg",
-  ];
-
-  //const urls = ["/shop", "/events", "/subscribe"];  // flip number 0 and 1 here for production
-  const urls = ["", "/events", "https://www.donnavino.dk/#subscribe"];
-
   // Logic for handling use of the prev/next-buttons
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(1);
 
   const handleNext = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % slides.length);
   };
 
   const handlePrevious = () => {
     setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1,
     );
   };
 
@@ -70,7 +71,7 @@ const HeroSlider = () => {
         </div>
       )}
       <div className="md:hidden flex mx-auto my-3 mt-6 relative top-3">
-        {Array.from({ length: images.length }).map((_, index) => (
+        {slides.map((_, index) => (
           <button
             key={index}
             onClick={() => handleDotClick(index)}
@@ -91,7 +92,7 @@ const HeroSlider = () => {
         ))}
       </div>
       <div className="relative justify-center self-start w-full md:w-1/2 min-h-[22.75rem] md:h-full">
-        {currentImageIndex === 1 ? ( // change this to 0 before production (after all other changes are made)
+        {currentImageIndex === 0 ? ( // change this to 0 before production (after all other changes are made)
           <video
             className="absolute inset-0 mt-6 md:mt-0 md:rounded-t-[0rem] md:rounded-tr-[8rem] md:rounded-br-xl object-cover w-full h-full"
             autoPlay
@@ -102,12 +103,12 @@ const HeroSlider = () => {
             aria-hidden="true"
             data-testid="hero-video"
           >
-            <source src={[images[currentImageIndex]]} type="video/mp4" />
+            <source src={slides[currentImageIndex].media} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         ) : (
           <img
-            src={images[currentImageIndex]}
+            src={slides[currentImageIndex].media}
             alt="Tasting session slide"
             className={`absolute inset-0 mt-6 md:mt-0 rounded-tl-[0rem] !rounded-tl-[0rem] ${currentImageIndex === 1 ? "md:rounded-tr-[8rem] md:rounded-br-xl" : "md:rounded-tl-[8rem] !md:rounded-tl-[8rem] md:rounded-bl-xl"} object-cover w-full h-full`}
             data-testid="hero-image"
@@ -117,23 +118,25 @@ const HeroSlider = () => {
       <div className="flex flex-col justify-center align-start items-start md:items-start font-barlow font-regular px-12 sm:px-20 md:px-6 lg:px-10 xl:px-14 min-h-[20rem] md:max-w-[50%]">
         <div>
           <p className="text-left sm:text-titleSmall md:text-titleMedium xl:text-titleLarge text-primary-normal">
-            {translations[subheadings[currentImageIndex]]}
+            {translations[slides[currentImageIndex].subheading]}
           </p>
         </div>
         <h2 className="text-displayMedium text-left md:text-left md:text-headlineLarge lg:text-displaySmall xl:text-displayMedium text-tertiary1-dark my-2 md:my-2 mr-8">
-          {translations[headings[currentImageIndex]]}
+          {translations[slides[currentImageIndex].heading]}
         </h2>
         <p className="text-start md:text-start text-bodyMedium md:text-bodyMedium xl:text-bodyLarge mt-2 mb-7 sm:mt-2 sm:mb-7 md:mt-2 mb-4 md:mb-5 xl:mb-10 mr-10">
-          {translations[paragraphs[currentImageIndex]]}
+          {translations[slides[currentImageIndex].paragraph]}
         </p>
         <Button
-          text={translations[buttons[currentImageIndex]]}
-          icon={buttonIcons[currentImageIndex]}
+          text={translations[slides[currentImageIndex].buttonText]}
+          icon={slides[currentImageIndex].buttonIcon}
           variant="redFullText"
-          aria-label={translations[buttons[currentImageIndex]]}
+          aria-label={translations[slides[currentImageIndex].buttonText]}
           data-testid="book-tasting-button"
-          linkUrl={urls[currentImageIndex]}
-          onClick={currentImageIndex === 0 ? () => setIsModalOpen(true) : ""}
+          linkUrl={slides[currentImageIndex].url}
+          onClick={
+            currentImageIndex === 0 ? () => setIsModalOpen(true) : undefined
+          }
         ></Button>
         <div
           className={`hidden md:flex mt-4 md:absolute md:bottom-8 lg:bottom-10 xl:bottom-14 ${currentImageIndex === 1 ? "md:right-8 lg:right-10 xl:right-12" : "md:left-[37.5%]"}`}
