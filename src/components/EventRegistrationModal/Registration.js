9592c38fc402367e486e408d878fd2bc2a503900
Alejandro-dev99 <80400286+Alejandro-dev99@users.noreply.github.com>
2025-04-19 08:59:19 +0200

    feat: refactor element, and included mobilepay api fetch

--- src/components/EventRegistrationModal/Registration.js
+++ src/components/EventRegistrationModal/Registration.js
@@ -17,7 +17,26 @@ function Registration({ eventDetails = {}, onClose }) {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+  const handlePayment = async () => {
+    const orderId = `event-${Date.now()}`;
+    const res = await fetch("/api/create-payment", {
+      method: "POST",
+      body: JSON.stringify({
+        orderId: orderId,
+        amount: Number(price) * Number(seats),
+      }), // 5000 = 50.00 DKK
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
+    const data = await res.json();
+    if (data.url) {
+      window.location.href = data.url; // Redirect to MobilePay payment page
+    } else {
+      alert("Something went wrong");
+    }
+  };
   return (
     <>
       <h3 className="text-headlineSmall text-center font-medium font-barlow mb-4">
@@ -166,6 +185,7 @@ function Registration({ eventDetails = {}, onClose }) {
               type="submit"
               className="w-full md:flex-1 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded font-medium disabled:bg-blue-500 disabled:hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
               disabled={!agree || availableSeats === 0}
+              onClick={handlePayment}
             >
               <span>{translations["event.registration.form.payWith"]}</span>
               <img
