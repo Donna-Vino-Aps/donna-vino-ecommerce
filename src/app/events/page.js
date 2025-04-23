f448400780a3a1e37813a83af08f8adee6e5955f
Natalia Lapina <lapinann08@gmail.com>
2025-04-23 09:01:33 +0200

    feat: add EventList component into page Events

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -4,12 +4,14 @@ import CalendarFrame from "@/components/Calendar/CalendarFrame";
 import EventsHeader from "@/components/Events/EventsHeader";
 import { EventsProvider } from "@/context/EventsContext";
 import React from "react";
+import EventList from "@/components/Events/EventList";
 
 const Events = () => {
   return (
     <EventsProvider>
       <EventsHeader />
       <CalendarFrame />
+      <EventList />
     </EventsProvider>
   );
 };
