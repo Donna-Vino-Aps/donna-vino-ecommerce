f4a33cdc2707919142c4372c2077b8719d413712
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-04-29 10:14:52 +0200

    fix: changing some variables to match updated NEXT_PUBLIC env prefix

--- src/config/shopify.js
+++ src/config/shopify.js
@@ -1,16 +1,16 @@
 import { logInfo, logError } from "@/utils/logging";
 
 // Shopify store configuration
-const SHOPIFY_STORE_DOMAIN = process.env.SHOPIFY_STORE_DOMAIN;
+const SHOPIFY_STORE_DOMAIN = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
 const SHOPIFY_STOREFRONT_ACCESS_TOKEN =
-  process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+  process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
 
 // Check if environment variables are set
 if (!SHOPIFY_STORE_DOMAIN) {
   logError("Error: SHOPIFY_DOMAIN environment variable is not set");
 }
 
-if (!SHOPIFY_STOREFRONT_ACCESS_TOKEN) {
+if (!NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN) {
   logError(
     "Error: SHOPIFY_STOREFRONT_ACCESS_TOKEN environment variable is not set",
   );
