c63119807b78e08bc71d49da0d3eeeac05961e62
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2025-05-05 14:24:09 +0200

    fixed issues with tailwind classes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -64,13 +64,13 @@ const Footer = () => {
   return (
     <footer
     
-      className="flex text-white text-center py-4 h-96 bg-[#2F2E2E] md:h-[26.625rem] h-[48.625rem] items-center justify-center"
+      className="flex h-96 h-[48.625rem] items-center justify-center bg-[#2F2E2E] py-4 text-center text-white md:h-[26.625rem]"
       data-testid="footer"
       aria-label="Footer"
     >
       <div>
-        <div className="flex flex-col relative items-center gap-1 md:bottom-3 bottom-14 md:mb-32 md:flex-row md:gap-6 lg:gap-9 xl:gap-12">
-          <Link href="/" className="navbar-brand" aria-label="logo">
+        <div className="relative bottom-14 flex flex-col items-center gap-1 md:bottom-3 md:mb-32 md:flex-row md:gap-6 lg:gap-9 xl:gap-12">
+          <Link href="/" aria-label="logo">
             <img
               className="relative mb-4 mt-6 h-[5.351rem] w-[7.75rem] rounded md:right-4 md:top-6 md:mb-0 md:mt-0"
               src="/images/donna-vino-logo-transparent.png"
@@ -86,7 +86,7 @@ const Footer = () => {
                   key={index}
                   data-testid={dataTestId}
                   href={href}
-                  className={`rounded-md px-3 py-2 text-bodyLarge text-semibold order-${index + 2} md:order-${index + 1}`}
+                  className={`rounded-md px-3 py-2 text-bodyLarge${index + 2}`}
                   role="navigation"
                   aria-label={`Link to ${label}`}
                 >
@@ -95,8 +95,8 @@ const Footer = () => {
               ))}
 
             </div>
-            <div className="md:left-3 flex order-1 md:order-1 relative md:top-20 top-10">
-              <div className="place-items-center md:flex grid grid-cols-2 gap-12 md:gap-9 lg:gap-14">
+            <div className="relative top-10 order-1 flex md:left-3 md:top-20 md:order-1">
+              <div className="grid grid-cols-2 place-items-center gap-12 md:flex md:gap-9 lg:gap-14">
                 {paymentIcons.map(({ src }, index) => (
                   <div key={index} className={index == paymentIcons.length - 1 ? "col-span-2" : "col-span-1"}><img
                     src={src}
@@ -106,8 +106,8 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col order-1 md:order-4 items-center relative md:top-4 top-20">
-            <h4 className="text-bodyLarge text-semibold mb-1 md:mb-3 md:mt-4">
+          <div className="relative top-20 order-1 flex flex-col items-center md:top-4 md:order-4">
+            <h4 className="mb-1 text-bodyLarge md:mb-3 md:mt-4">
               {translations["footer.follow"]}
             </h4>
             <div
@@ -132,7 +132,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="pt-3 top-20 relative whitespace-nowrap text-bodySmall md:text-bodyMedium text-tertiary2-hover_dark"><p className="company-number">© 2025 Donna Vino Aps | CVR-n. 45017567 | <a className="underline" href="https://www.donnavino.dk/privacy-policy" role="navigation" aria-label="Link to Privacy Policy">Privacy Policy</a></p></div>
+        <div className="relative top-20 whitespace-nowrap pt-3 text-bodySmall text-tertiary2-hover_dark md:text-bodyMedium"><p>© 2025 Donna Vino Aps | CVR-n. 45017567 | <a className="underline" href="https://www.donnavino.dk/privacy-policy" role="navigation" aria-label="Link to Privacy Policy">Privacy Policy</a></p></div>
     </div>
     </footer>
   );
