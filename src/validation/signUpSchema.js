7c052c49a49dde95d294f7ddedef855bc5856fb7
Natalia Lapina <lapinann08@gmail.com>
2025-05-12 13:31:11 +0200

    fix: add periods to validation messages and hints and improve Danish translations

--- src/validation/signUpSchema.js
+++ src/validation/signUpSchema.js
@@ -21,61 +21,61 @@ export const createSignUpSchema = (translations) => {
 
   return Yup.object({
     firstName: Yup.string().required(
-      translations["signUp.validation.required"] || "This field is required",
+      translations["signUp.validation.required"] || "This field is required.",
     ),
     lastName: Yup.string().required(
-      translations["signUp.validation.required"] || "This field is required",
+      translations["signUp.validation.required"] || "This field is required.",
     ),
     email: Yup.string()
       .matches(
         /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
         translations["signUp.validation.emailFormat"] ||
-          "Please enter a valid email address",
+          "Please enter a valid email address.",
       )
       .required(
-        translations["signUp.validation.required"] || "This field is required",
+        translations["signUp.validation.required"] || "This field is required.",
       ),
     confirmEmail: Yup.string()
       .oneOf(
         [Yup.ref("email"), null],
         translations["signUp.validation.emailMatch"] ||
-          "Emails do not match. Please check and try again",
+          "Emails do not match. Please check and try again.",
       )
       .required(
-        translations["signUp.validation.required"] || "This field is required",
+        translations["signUp.validation.required"] || "This field is required.",
       ),
     password: Yup.string()
       .matches(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
         translations["signUp.validation.passwordFormat"] ||
-          "Password must be at least 8 characters long, including one uppercase letter, one lowercase letter, one number, and one special character (e.g., !, @, #, $)",
+          "Password must be at least 8 characters long, including one uppercase letter, one lowercase letter, one number, and one special character (e.g., !, @, #, $).",
       )
       .required(
-        translations["signUp.validation.required"] || "This field is required",
+        translations["signUp.validation.required"] || "This field is required.",
       ),
     confirmPassword: Yup.string()
       .oneOf(
         [Yup.ref("password"), null],
         translations["signUp.validation.passwordMatch"] ||
-          "Passwords do not match. Please check and try again",
+          "Passwords do not match. Please check and try again.",
       )
       .required(
-        translations["signUp.validation.required"] || "This field is required",
+        translations["signUp.validation.required"] || "This field is required.",
       ),
     birthdate: Yup.string()
       .required(
-        translations["signUp.validation.required"] || "This field is required",
+        translations["signUp.validation.required"] || "This field is required.",
       )
       .test(
         "age-validation",
         translations["signUp.validation.ageRequirement"] ||
-          "You must be at least 18 years old to register",
+          "You must be at least 18 years old to register.",
         validateBirthdate,
       ),
     acceptTerms: Yup.boolean().oneOf(
       [true],
       translations["signUp.validation.acceptTerms"] ||
-        "This checkbox is required",
+        "This checkbox is required.",
     ),
   });
 };
