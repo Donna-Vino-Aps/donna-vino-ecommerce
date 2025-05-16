ede0bd7f6fda5a75102f78961dc5b897eafa99a6
Natalia Lapina <lapinann08@gmail.com>
2025-05-16 09:06:51 +0200

    chore(tailwind): include utils directory in content configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ export default {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}", // Add this line
   ],
   theme: {
     extend: {
