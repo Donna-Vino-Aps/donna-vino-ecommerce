c011a22ae913e4d8bd2ec2433f181da6ed205bae
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2025-05-21 13:55:34 +0200

    feat: umami

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import TastingSession from "@/components/HeroSlider/HeroSlider";
 import ServicesBanner from "@/components/ServicesBanner/ServicesBanner";
 import SalesCards from "@/components/SalesCards/SalesCards";
+
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.dataset.websiteId = "2a60d0b9-2baa-48f9-88df-67e44d159e85";
+    script.src = "https://cloud.umami.is/script.js";
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Clean up on unmount
+    };
+  }, []);
+
   return (
     <div className="flex w-[100%]" data-testid="home-container">
       <main
