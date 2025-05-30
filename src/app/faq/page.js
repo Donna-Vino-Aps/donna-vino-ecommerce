f8e410e0c8c25b2cdf5598d2d7761ef0779df2ff
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-05-30 11:37:32 +0200

    feat: importing component from eventsheader and changing translations.

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -9,17 +9,18 @@ const FrequentlyAskedQuestions = () => {
 
   return (
     <div>
-      <section className="flex h-[7.75rem] w-full flex-row md:h-[29.313rem]">
-        <div>
-          <h1>Customer FAQs</h1>
-          <p>Here you can find the most searched questions by our clients.</p>
+      <section className="z-[1] flex flex-row bg-primary-light">
+        <div className="mb-4 flex flex-col justify-center gap-6 px-8 text-center sm:mb-0 sm:w-[45%] md:text-start lg:w-[40%]">
+          <h1 className="mt-8 text-displayMedium sm:mt-4 sm:max-w-full sm:text-displaySmall md:mt-0 lg:text-displayMedium">
+            {translations["faq.h1"]}
+          </h1>
+          <p className="mb-4 text-bodyLarge sm:mb-0">{translations["faq.p"]}</p>
         </div>
-        <figure className="relative h-[29.313rem] w-[650px]">
-          <Image
-            className="object-cover object-right "
-            alt="Pouring a glass of wine at a wine tasting"
-            src="/images/faq-image.png"
-            fill
+        <figure className="hidden overflow-hidden object-right sm:flex sm:min-w-[55%] lg:min-w-[60%]">
+          <img
+            src="/images/events-header-unsplash.jpg"
+            className="min-h-[22.5rem] w-full object-cover sm:max-h-[25.313rem] sm:min-w-[54.375rem] sm:rounded-tl-[11.531rem] md:max-h-[29.313rem]"
+            alt="Hands pouring a glass of wine at a wine tasting"
           />
         </figure>
       </section>
