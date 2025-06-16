d6d21aed495be21d34a8af6f5ff1dff6ebb5a655
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-06-16 09:03:24 +0200

    Relocate a Pairings component to Wines page

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,9 @@
 "use client";
+
 import React from "react";
 import TastingSession from "@/components/HeroSlider/HeroSlider";
 import ServicesBanner from "@/components/ServicesBanner/ServicesBanner";
 import SalesCards from "@/components/SalesCards/SalesCards";
-import Pairings from "@/components/Pairing/Pairings";
-
-const pairingSet = [
-  {
-    img: "images/pairing/ribeye.png",
-    title: "Grilled Ribeye steak",
-    description:
-      "The bold tannins of this wine balance the juiciness of the meat.",
-  },
-  {
-    img: "images/pairing/pasta.png",
-    title: "Truffle pasta",
-    description:
-      "Earthy flavors in perfect harmony. Lorem ipsum ubit sun amet.",
-  },
-  {
-    img: "images/pairing/pecorino.png",
-    title: "Aged Pecorino cheese",
-    description: "Its saltiness enhances the wine’s fruity notes.",
-  },
-  {
-    img: "images/pairing/tuna.png",
-    title: "Seared Tuna steak",
-    description: "A refined contrast of richness and freshness.",
-  },
-];
 
 export default function Home() {
   return (
@@ -39,7 +14,6 @@ export default function Home() {
       >
         <TastingSession />
         <ServicesBanner />
-        <Pairings {...{ pairingSet }} />
         <SalesCards />
       </main>
     </div>
--- src/app/wines/page.js
+++ src/app/wines/page.js
@@ -1,11 +1,38 @@
 "use client";
 
 import React from "react";
+import Pairings from "@/components/Pairing/Pairings";
+
+const pairingSet = [
+  {
+    img: "images/pairing/ribeye.png",
+    title: "Grilled Ribeye steak",
+    description:
+      "The bold tannins of this wine balance the juiciness of the meat.",
+  },
+  {
+    img: "images/pairing/pasta.png",
+    title: "Truffle pasta",
+    description:
+      "Earthy flavors in perfect harmony. Lorem ipsum ubit sun amet.",
+  },
+  {
+    img: "images/pairing/pecorino.png",
+    title: "Aged Pecorino cheese",
+    description: "Its saltiness enhances the wine’s fruity notes.",
+  },
+  {
+    img: "images/pairing/tuna.png",
+    title: "Seared Tuna steak",
+    description: "A refined contrast of richness and freshness.",
+  },
+];
 
 const Wines = () => {
   return (
     <div>
       <h1 className="text-center text-displayLarge">Wines</h1>
+      <Pairings {...{ pairingSet }} />
     </div>
   );
 };
