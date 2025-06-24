cfbff6b380842c042f85e73762faed5472f1a36e
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-06-24 20:59:45 +0200

    feat: inserting styling elements dotted shape (positioning not done yet) + updating responsive widths of FAQItem

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useLanguage } from "@/context/LanguageContext";
 import useIsMobile from "@/hooks/useIsMobile";
 import FaqSection from "@/components/FAQ/FaqSection";
+import Image from "next/image";
 // import Image from "next/image";
 
 const FrequentlyAskedQuestions = () => {
@@ -12,6 +13,20 @@ const FrequentlyAskedQuestions = () => {
   return (
     <div className={`${isMobile ? "mb-12 space-y-12" : "mb-14 space-y-14"}`}>
       <section className="z-[1] flex flex-row bg-primary-light">
+        <div>
+          <Image
+            src="/design-elements/dotted-shape.svg"
+            width="50"
+            height="50"
+            className="absolute left-0 top-[16.3rem] md:top-[41rem]"
+          />
+          <Image
+            src="/design-elements/dotted-shape.svg"
+            width="50"
+            height="50"
+            className="absolute right-0 top-[7.3rem] md:top-60"
+          />
+        </div>
         <div className="mb-4 flex flex-col justify-center gap-6 px-8 text-center sm:mb-0 sm:w-[45%] md:text-start lg:w-[40%]">
           <h1 className="mt-8 text-displayMedium sm:mt-4 sm:max-w-full sm:text-displaySmall md:mt-0 lg:text-displayMedium">
             {isMobile ? translations["faq.h1-mobile"] : translations["faq.h1"]}
--- src/components/FAQ/FaqItem.js
+++ src/components/FAQ/FaqItem.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import useIsMobile from "@/hooks/useIsMobile";
 import PropTypes from "prop-types";
 
 const FaqItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const isMobile = useIsMobile(768);
   return (
     <div
-      className={`mx-auto mb-3 flex cursor-pointer flex-col rounded-lg border border-solid border-[#D9D9D9] ${isOpen ? "bg-transparent" : "bg-tertiary2-normal"} ${isMobile ? "w-[18.5rem]" : "w-[48.125rem]"}`}
+      className={`mx-auto mb-3 flex cursor-pointer flex-col rounded-lg border border-solid border-[#D9D9D9] ${isOpen ? "bg-transparent" : "bg-tertiary2-normal"} w-[18.5rem] md:w-[42.125rem] lg:w-[48.125rem]`}
       onClick={() => setIsOpen((prev) => !prev)}
     >
       <div className="my-3 flex w-full items-center justify-between px-4 py-[3px]">
