799c9d30d0491cd2a3745eb5aa209878e5114f5e
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-06-25 16:20:13 +0200

    feat: create basic cart page

---
+++ src/app/cart/page.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import SEO from "@/components/SEO/SEO";
+
+const CartPage = () => {
+  return (
+    <div>
+      <SEO title="Cart" description="Cart" />
+      <h1 className="text-center text-headlineLarge">Grapes & Zones</h1>
+    </div>
+  );
+};
+
+export default CartPage;
