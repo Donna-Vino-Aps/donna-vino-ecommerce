5ca9fac3a949beeffc5acf40860111971109b336
Natalia Lapina <lapinann08@gmail.com>
2025-07-02 11:28:15 +0200

    refactor: update cart item component to use cart context

--- src/components/Cart/ModalCartItem.js
+++ src/components/Cart/ModalCartItem.js
@@ -2,46 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import Image from "next/image";
 import { QuantitySelector } from "../QuantitySelector/QuantitySelector";
+import { useCart } from "@/context/ShoppingCartContext";
 
-const ModalCartItem = ({
-  item,
-  removeCartItem,
-  setTotalQuantityInCart,
-  updateCartItemQuantity,
-  nrOfCartItems,
-}) => {
-  const [selectedQuantity, setSelectedQuantity] = React.useState(
-    item.quantitySelected,
-  );
-
+const ModalCartItem = ({ item, removeCartItem, updateCartItemQuantity }) => {
+  const { items } = useCart();
   return (
     <div className="mb-4 flex items-center justify-between font-barlow">
       <div className="flex items-center">
         <img
-          src={item.image}
-          alt={item.name}
+          src={item.imageUrl}
+          alt={item.variantTitle}
           className="relative top-1 h-20 w-20 object-cover"
         />
         <div className="ml-5 mt-1 flex flex-col gap-1">
           <h3 className="text-titleSmall font-medium md:text-titleMedium">
-            {item.name}
+            {item.variantTitle}
           </h3>
           <QuantitySelector
             item={item}
             quantityAvailable={item.quantityAvailable}
-            selectedQuantity={selectedQuantity}
-            setSelectedQuantity={setSelectedQuantity}
-            setTotalQuantityInCart={setTotalQuantityInCart}
+            selectedQuantity={item.quantity}
             updateCartItemQuantity={updateCartItemQuantity}
             preSale={item.preSale}
           />
         </div>
       </div>
       <div
-        className={`${nrOfCartItems > 2 ? "relative right-4" : ""} mt-3 flex flex-col justify-center gap-2`}
+        className={`${items.length > 2 ? "relative right-4" : ""} mt-3 flex flex-col justify-center gap-2`}
       >
         <button
-          onClick={() => removeCartItem(item.id)}
+          onClick={() => removeCartItem(item.variantId)}
           className="flex justify-end"
         >
           <Image
@@ -52,7 +42,7 @@ const ModalCartItem = ({
           />
         </button>
         <p className="text-titleMedium font-medium text-tertiary1-dark">
-          {(item.quantitySelected * item.price).toFixed(2).replace(".", ",")} kr
+          {(item.quantity * item.price).toFixed(2).replace(".", ",")} kr
         </p>
       </div>
     </div>
@@ -63,16 +53,14 @@ export default ModalCartItem;
 
 ModalCartItem.propTypes = {
   item: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
+    variantId: PropTypes.number.isRequired,
+    variantTitle: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
     quantityAvailable: PropTypes.number.isRequired,
-    quantitySelected: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired,
     preSale: PropTypes.bool,
   }).isRequired,
   removeCartItem: PropTypes.func.isRequired,
-  setTotalQuantityInCart: PropTypes.func.isRequired,
   updateCartItemQuantity: PropTypes.func.isRequired,
-  nrOfCartItems: PropTypes.number.isRequired,
 };
