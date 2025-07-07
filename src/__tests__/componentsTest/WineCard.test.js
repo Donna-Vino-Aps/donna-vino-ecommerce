98224c7fa5674ac2c50ea7fa8f0ab1069f44d94a
AnastasiaSkachenko <skachenkoa@gmail.com>
2025-07-07 12:17:24 +0200

    made add to cart button functional + break down WineCard into smaller components

--- src/__tests__/componentsTest/WineCard.test.js
+++ src/__tests__/componentsTest/WineCard.test.js
@@ -1,34 +1,69 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import WineCard from "@/components/Card/WineCard";
 import { LanguageProvider } from "@/context/LanguageContext";
+import { CartProvider } from "@/context/ShoppingCartContext";
 
-describe("WineCardSmall Component", () => {
-  const mockProps = {
-    title: "Château Margaux",
-    price: 0.0,
-    imageUrl: "/images/exampleImageWine.png",
-  };
+const mockWine = {
+  title: "Château Margaux",
+  bottlePrice: 150.0,
+  imageUrl: "/images/exampleImageWine.png",
+  rating: 4.7,
+  nrOfRatings: 134,
+  variantMap: {
+    bottle: {
+      id: "variant123",
+      price: {
+        amount: 150.0,
+        currencyCode: "DKK",
+      },
+    },
+  },
+};
 
-  test("renders WineCardSmall with title, price, and image", () => {
-    render(
+describe("WineCard Component", () => {
+  const renderWithProviders = (ui) => {
+    return render(
       <LanguageProvider>
-        <WineCard {...mockProps} />
+        <CartProvider>{ui}</CartProvider>
       </LanguageProvider>,
     );
+  };
 
-    expect(screen.getByTestId("wine-title")).toHaveTextContent(mockProps.title);
-    expect(screen.getByTestId("wine-price")).toHaveTextContent(mockProps.price);
+  test("renders wine title, price, and image", () => {
+    renderWithProviders(<WineCard wine={mockWine} context="pre-sale" />);
+
+    expect(screen.getByTestId("wine-title")).toHaveTextContent(mockWine.title);
+    expect(screen.getByTestId("wine-price")).toHaveTextContent("Kr. 150,00");
     expect(screen.getByTestId("wine-image")).toBeInTheDocument();
   });
 
   test("renders 'New' badge when isNew is true", () => {
-    render(
-      <LanguageProvider>
-        <WineCard {...mockProps} isNew={true} />
-      </LanguageProvider>,
+    renderWithProviders(<WineCard wine={mockWine} isNew context="pre-sale" />);
+    expect(screen.getByText("New")).toBeInTheDocument();
+  });
+
+  test("disables add-to-cart in top-wines context", () => {
+    const consoleWarnSpy = jest
+      .spyOn(console, "warn")
+      .mockImplementation(() => {});
+    renderWithProviders(<WineCard wine={mockWine} context="top-wines" />);
+
+    const button = screen.getByTestId("addToCart-button");
+    fireEvent.click(button);
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "Pressed add to cart in TopWines section",
     );
 
-    expect(screen.getByText("New")).toBeInTheDocument();
+    consoleWarnSpy.mockRestore();
+  });
+
+  test("adds wine to cart in pre-sale context", () => {
+    renderWithProviders(<WineCard wine={mockWine} context="pre-sale" />);
+    const button = screen.getByTestId("addToCart-button");
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent(/Added to Cart/i);
   });
 });
---
+++ src/components/Card/WineActions.js
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Button from "../Button/Button.js";
+import { useLanguage } from "@/context/LanguageContext";
+import { useCart } from "@/context/ShoppingCartContext.js";
+import useIsMobile from "@/hooks/useIsMobile.js";
+
+const WineActions = ({ wine, isPreSale }) => {
+  const { translations } = useLanguage();
+  const { addItemToCart } = useCart();
+  const [isClicked, setIsClicked] = useState(false);
+  const isMobile = useIsMobile();
+
+  const handleAddToCart = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsClicked(true);
+
+    const bottle = wine.variantMap.bottle;
+
+    const itemToAdd = {
+      title: wine.title,
+      variantId: bottle.id,
+      size: "bottle",
+      price: bottle.price.amount,
+      currencyCode: bottle.price.currencyCode,
+      quantity: 1,
+      imageUrl: wine.imageUrl,
+    };
+
+    addItemToCart(itemToAdd);
+    setTimeout(() => setIsClicked(false), 3000);
+  };
+
+  return (
+    <div className="flex w-full items-end justify-between">
+      <p
+        data-testid="wineCard-viewDetails"
+        aria-label="Go to home"
+        className={`cursor-pointer  text-bodySmall text-black underline ${isPreSale && "sm:text-bodyMedium"}`}
+      >
+        {translations["wineCard.viewDetails"]}
+      </p>
+      <Button
+        text={
+          translations[
+            isClicked ? "wineCard.addedToCart" : "wineCard.addToCart"
+          ]
+        }
+        onClick={
+          isPreSale
+            ? handleAddToCart
+            : () => console.warn("Pressed add to cart in TopWines section")
+        }
+        size={isPreSale && !isMobile ? "lg" : "md"}
+        width={isPreSale && !isMobile ? "lg" : "md"}
+        ariaLabel={"Add wine to cart"}
+        testId="addToCart-button"
+        icon={"/icons/card/cart-white.svg"}
+        extraStyle={`
+          px-4 py-2  text-nowrap text-white text-titleMedium font-medium 
+          transition-transform duration-150 ease-in-out active:scale-95
+          ${isClicked && "bg-others-confirm hover:bg-others-confirm"} ${isPreSale && "sm:px-5 sm:py-3"}
+        `}
+      />
+    </div>
+  );
+};
+
+WineActions.propTypes = {
+  wine: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    bottlePrice: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    nrOfRatings: PropTypes.number.isRequired,
+    variantMap: PropTypes.shape({
+      bottle: PropTypes.object,
+    }).isRequired,
+  }).isRequired,
+  isPreSale: PropTypes.bool.isRequired,
+};
+
+export default WineActions;
