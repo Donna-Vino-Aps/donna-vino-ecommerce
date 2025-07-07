c5bc1b7d4c55ca2cd32d252584d3fe0d6e0af33f
AnastasiaSkachenko <skachenkoa@gmail.com>
2025-07-07 13:41:44 +0200

    fix: stable color change

--- src/components/Card/WineActions.js
+++ src/components/Card/WineActions.js
@@ -53,16 +53,13 @@ const WineActions = ({ wine, isPreSale }) => {
             ? handleAddToCart
             : () => console.warn("Pressed add to cart in TopWines section")
         }
+        color={isClicked ? "" : "primary"}
         size={isPreSale && !isMobile ? "lg" : "md"}
         width={isPreSale && !isMobile ? "lg" : "md"}
         ariaLabel={"Add wine to cart"}
         testId="addToCart-button"
         icon={"/icons/card/cart-white.svg"}
-        extraStyle={`
-          px-4 py-2  text-nowrap text-white text-titleMedium font-medium 
-          transition-transform duration-150 ease-in-out active:scale-95
-          ${isClicked && "bg-others-confirm hover:bg-others-confirm"} ${isPreSale && "sm:px-5 sm:py-3"}
-        `}
+        extraStyle={`px-4 py-2 text-nowrap text-white text-titleMedium font-medium transition-transform duration-150 ease-in-out  ${isClicked ? " bg-others-confirm hover:bg-others-confirm " : ""} active:scale-95 ${isPreSale && " sm:px-5 sm:py-3 "}`}
       />
     </div>
   );
