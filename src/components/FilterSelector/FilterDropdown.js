54b4b53c06bcb8e9e8579ae750901f17d9e137b6
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-07-09 15:08:11 +0200

    fix: making chevron-icon clickable (still some space around is not clickable)

--- src/components/FilterSelector/FilterDropdown.js
+++ src/components/FilterSelector/FilterDropdown.js
@@ -20,25 +20,29 @@ const FilterDropdown = ({ filter }) => {
   return (
     <div className="flex flex-col">
       <div
-        className={`flex cursor-pointer items-center justify-between border border-tertiary1-light px-3 md:px-5 ${isFilterOpen ? "rounded-t-lg" : "rounded-lg shadow-md"} h-[3.75rem] md:w-[19rem] lg:w-[23rem] 3xl:w-[26rem]`}
+        className={`flex cursor-pointer items-center justify-between border-b border-l border-r border-tertiary1-light px-5 ${isFilterOpen ? "rounded-t-lg" : "rounded-lg shadow-md"} h-[3.75rem] md:w-[19rem] lg:w-[23rem] 3xl:w-[26rem]`}
         onClick={() => setIsFilterOpen((prev) => !prev)}
       >
         <p className="font-barlow text-headlineSmall font-normal text-tertiary2-darker md:text-titleLarge">
           {filter.title}
         </p>
-        <Image
-          src={
-            isFilterOpen ? "/icons/chevron-up.svg" : "/icons/chevron-down.svg"
-          }
-          alt={isFilterOpen ? "Close filter dropdown" : "Open filter dropdown"}
-          width={28}
-          height={28}
-          className="h-7 w-7"
-        />
+        <div className="relative h-7 w-7 cursor-pointer">
+          <Image
+            src={
+              isFilterOpen ? "/icons/chevron-up.svg" : "/icons/chevron-down.svg"
+            }
+            alt={
+              isFilterOpen ? "Close filter dropdown" : "Open filter dropdown"
+            }
+            width={28}
+            height={28}
+            className="pointer-events-auto h-7 w-7 cursor-pointer object-contain"
+          />
+        </div>
       </div>
       {isFilterOpen &&
         (filter.variant === "regular" ? (
-          <div className="mb-2 h-auto gap-3 rounded-b-lg border-b border-l border-r border-tertiary1-light py-3 shadow-sm">
+          <div className="mb-2 h-auto gap-3 rounded-b-lg border-b border-l border-r border-tertiary1-light py-3 shadow-sm md:w-[19rem] lg:w-[23rem] 3xl:w-[26rem]">
             {filter.options.map((option) => (
               <div
                 key={option}
@@ -55,7 +59,7 @@ const FilterDropdown = ({ filter }) => {
             ))}
           </div>
         ) : (
-          <div className="flex h-[8.375rem] flex-col rounded-b-lg border-b border-l border-r border-tertiary1-light shadow-md md:h-[10.5rem]">
+          <div className="flex h-[8.375rem] flex-col rounded-b-lg border-b border-l border-r border-tertiary1-light shadow-md md:h-[10.5rem] md:w-[19rem] lg:w-[23rem] 3xl:w-[26rem]">
             <div className="flex justify-between px-8 py-8">
               <div className="flex flex-col">
                 <p className="text-titleSmall text-others-gray">
