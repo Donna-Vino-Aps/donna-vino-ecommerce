2f65029578412fc64e61ef9d264ce7a681bc8837
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-07-10 15:05:36 +0200

    fix: lint-fix

--- src/components/FAQ/FaqItem.js
+++ src/components/FAQ/FaqItem.js
@@ -6,7 +6,7 @@ const FaqItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div
-      className={`mx-auto flex flex-col rounded-lg border border-solid border-tertiary2-hover_normal ${isOpen ? "bg-transparent" : "bg-tertiary2-normal"} w-full sm:max-w-[48.125rem] max-w-[18.5rem] sm:min-w-[18.5rem] sm:mx-8 px-4`}
+      className={`mx-auto flex flex-col rounded-lg border border-solid border-tertiary2-hover_normal ${isOpen ? "bg-transparent" : "bg-tertiary2-normal"} w-full max-w-[18.5rem] px-4 sm:mx-8 sm:min-w-[18.5rem] sm:max-w-[48.125rem]`}
     >
       <article
         className="my-3 flex w-full cursor-pointer items-center justify-between py-[3px]"
