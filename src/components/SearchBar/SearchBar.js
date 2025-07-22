6ca6ff78825ac3ad969274f9440b0e24fc2ce02d
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-07-22 12:45:32 +0200

    remove import which is not used anymore

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import useIsMobile from "@/hooks/useIsMobile";
 import SearchButtonFilter from "./SearchButtonFilter";
 import SortBy from "../FilterSelector/SortBy";
