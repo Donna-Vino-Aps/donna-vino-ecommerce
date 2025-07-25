520e9b3d453b58bb18c075f1a719199fea5b1fe3
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-07-25 13:53:03 +0200

    feat: add ReviewsCard component to render list of Review items

--- src/components/Reviews/ReviewsCard.js
+++ src/components/Reviews/ReviewsCard.js
@@ -1,7 +1,23 @@
 "use client";
 
 import React from "react";
+import Review from "@/components/Reviews/Review";
+import PropTypes from "prop-types";
 
-export default function ReviewsCard() {
-  return <div>Reviews</div>;
+export default function ReviewsCard({ reviews }) {
+  if (!reviews.length) {
+    return <p>No reviews yet.</p>;
+  }
+
+  return (
+    <section>
+      {reviews.map((review, id) => (
+        <Review key={id} {...review} />
+      ))}
+    </section>
+  );
 }
+
+ReviewsCard.propTypes = {
+  reviews: PropTypes.arrayOf(PropTypes.shape(Review.propTypes)).isRequired,
+};
