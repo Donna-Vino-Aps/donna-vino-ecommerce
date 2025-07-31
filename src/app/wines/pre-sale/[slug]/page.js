2feccb3dc09853ad4b739e1fca8c18ef2a80bd6d
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2025-07-31 15:28:37 +0200

    fixed code style issue

--- src/app/wines/pre-sale/[slug]/page.js
+++ src/app/wines/pre-sale/[slug]/page.js
@@ -43,7 +43,7 @@ export default function WineDetailPage() {
       <WineInfo wine={wine} />
       <MoreInfoDropdown wine={wine} />
       <TasteProfile wine={wine} />
-      <TasteProfileCards/>
+      <TasteProfileCards />
     </div>
   );
 }
