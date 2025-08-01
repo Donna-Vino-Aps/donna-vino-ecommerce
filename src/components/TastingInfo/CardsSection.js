b245690dbfb27ffd3e83147c2e423baf986dc558
AnastasiaSkachenko <skachenkoa@gmail.com>
2025-08-01 10:18:11 +0200

    adaptions to all screen sizes

--- src/components/TastingInfo/CardsSection.js
+++ src/components/TastingInfo/CardsSection.js
@@ -2,8 +2,7 @@
 import { useLanguage } from "@/context/LanguageContext";
 import Image from "next/image";
 import React from "react";
-import Button from "../Button/Button";
-import useIsMobile from "@/hooks/useIsMobile";
+import TastingCard from "./TastingCard";
 
 const events = [
   {
@@ -31,67 +30,24 @@ const events = [
 
 export default function CardsSection() {
   const { translations } = useLanguage();
-  const isMobile = useIsMobile();
 
   return (
     <section className="flex w-full items-center justify-center pb-20">
       <div className=" max-w-7xl">
-        <h1
-          className={`my-6 text-center sm:my-9  ${isMobile ? "text-headlineSmall" : "text-displaySmall"}`}
-        >
+        <h1 className="mb-6 text-center font-roboto text-headlineSmall sm:mb-16 sm:text-displaySmall">
           {translations["tasting-info.cardsHeader"]}
         </h1>
-        {isMobile && (
-          <Image
-            src={"/icons/chevron-down.svg"}
-            height={20}
-            width={20}
-            className="mx-auto mb-6"
-            alt="events arrow"
-          />
-        )}
-        <div
-          className={`flex  justify-center gap-7 ${isMobile ? "flex-col items-center" : "sm:flex-row sm:items-stretch"}`}
-        >
+        <Image
+          src={"/icons/chevron-down.svg"}
+          height={20}
+          width={20}
+          className="visible mx-auto mb-6 sm:invisible"
+          alt=""
+          aria-hidden="true"
+        />
+        <div className="flex flex-col items-center justify-center gap-7 xl:flex-row">
           {events.map((event) => (
-            <div
-              key={event.title}
-              className={`flex w-72 flex-col gap-5 bg-white pb-5 sm:w-96 ${isMobile ? "rounded-[32px]" : "rounded-[50px]"}`}
-            >
-              <Image
-                src={"/images/" + event.image}
-                width={380}
-                height={160}
-                className="h-40 w-96"
-                alt={event.title}
-              />
-
-              <h3
-                className={`text-center ${isMobile ? "text-headlineSmall" : "text-headlineLarge"}`}
-              >
-                {translations["tasting-info." + event.title]}
-              </h3>
-              <div
-                className={`flex flex-col justify-between px-3 ${isMobile ? "h-32" : "h-44"}`}
-              >
-                <p
-                  className={`text-center ${isMobile && "text-labelSmall"}`}
-                  dangerouslySetInnerHTML={{
-                    __html: translations["tasting-info-card." + event.body],
-                  }}
-                ></p>
-                <p className={`text-center ${isMobile && "text-labelSmall"}`}>
-                  {translations["tasting-info-card." + event.engagement]}
-                </p>
-              </div>
-
-              <Button
-                text={translations["tasting-info-card." + event.buttonTitle]}
-                onClick={() => console.warn(event.title, " button clicked")}
-                extraStyle="mx-auto min-w-max px-3"
-                size={isMobile ? "sm" : "md"}
-              />
-            </div>
+            <TastingCard key={event.title} {...event} />
           ))}
         </div>
       </div>
--- src/components/TastingInfo/HeroSection.js
+++ src/components/TastingInfo/HeroSection.js
@@ -1,6 +1,5 @@
 "use client";
 import { useLanguage } from "@/context/LanguageContext";
-import useIsMobile from "@/hooks/useIsMobile";
 import Image from "next/image";
 import React from "react";
 
@@ -12,34 +11,29 @@ const events = [
 
 export default function HeroSection() {
   const { translations } = useLanguage();
-  const isMobile = useIsMobile();
 
   return (
-    <section className="flex w-full items-center justify-center pt-10">
-      <div
-        className={`flex max-w-7xl flex-col-reverse items-center sm:flex-row ${isMobile ? "gap-8" : "gap-16"} `}
-      >
-        <div className={`max-w-[45rem] ${isMobile && "m-5"}`}>
+    <section className="flex w-full items-center justify-center pb-4 pt-10">
+      <div className="flex max-w-7xl flex-col-reverse items-center gap-8 lg:flex-row  xl:gap-16">
+        <div className="m-5 max-w-[80%] lg:max-w-[40rem] xl:max-w-[45rem]">
           <h1
-            className={`text-tertiary1-dark ${isMobile ? "text-center text-displaySmall" : "text-displayLarge"} `}
+            className={`text-center font-roboto text-displaySmall text-tertiary1-dark sm:text-start md:text-displayLarge`}
           >
             {translations["tasting-info.heroHeader"]}
           </h1>
           <p
-            className={`mt-2 text-tertiary1-dark ${isMobile ? "text-center text-bodySmall" : "text-bodyLarge"}`}
+            className={`mt-2 text-center text-bodySmall text-tertiary1-dark sm:text-start md:text-bodyLarge`}
             dangerouslySetInnerHTML={{
               __html: translations["tasting-info.heroBody"],
             }}
           ></p>
           {events.map((event) => (
             <div key={event.title} className="mt-5 flex flex-row items-start">
-              <h2
-                className={`me-2 flex-shrink-0 font-bold text-primary-normal ${isMobile ? "text-bodyMedium" : "text-bodyLarge"}`}
-              >
+              <h2 className="me-2 flex-shrink-0 text-bodyMedium font-bold text-primary-normal sm:text-bodyLarge">
                 {translations["tasting-info." + event.title]}:
               </h2>
               <p
-                className={`${isMobile && "text-bodySmall"} ${event.title == "tastingClassHeader" && "text-primary-normal"}`}
+                className={`text-bodyLarge sm:text-bodySmall ${event.title == "tastingClassHeader" && "text-primary-normal"}`}
                 dangerouslySetInnerHTML={{
                   __html: translations["tasting-info." + event.description],
                 }}
@@ -47,30 +41,28 @@ export default function HeroSection() {
             </div>
           ))}
         </div>
-        <div
-          className={`relative flex w-[200px] items-center justify-center sm:w-[300px] lg:w-[440px] ${isMobile && "mt-5"}`}
-        >
+        <div className="relative my-6 flex w-[200px] items-center justify-center sm:w-[300px] lg:my-0 xl:w-[440px]">
           <Image
             src="/images/image-bg-shapes.svg"
-            width={isMobile ? 200 : 440}
-            height={isMobile ? 200 : 440}
+            width={440}
+            height={440}
             alt="image-background"
-            className={`absolute ${isMobile ? "h-52 w-52" : "h-[440px] w-[440px]"}`}
+            className="absolute h-52 w-52 sm:h-72 sm:w-72 xl:h-[440px] xl:w-[440px]"
           />
           <Image
             src={"/images/hero-tasting-info.jpg"}
-            width={isMobile ? 120 : 265}
-            height={isMobile ? 160 : 318}
-            className={`absolute rounded-lg ${isMobile ? "h-36 w-32" : "h-[318px] w-[265px]"}`}
+            width={265}
+            height={318}
+            className="absolute h-36 w-32 rounded-lg sm:h-48 sm:w-40 xl:h-[318px] xl:w-[265px]"
             alt="wines"
           />
 
           <Image
             src={"/images/ellipse-tasting-info.png"}
-            width={isMobile ? 130 : 280}
-            height={isMobile ? 140 : 370}
+            width={280}
+            height={370}
             alt="glow"
-            className={isMobile ? "h-36 w-28" : "h-64 w-96"}
+            className="h-36 w-28 sm:h-48 sm:w-36 xl:h-80 xl:w-64"
             style={{ mixBlendMode: "overlay" }}
           />
         </div>
