e555f6d554c6c466235b3713dc05eff11acfbd87
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2025-08-11 10:45:47 +0200

    form added

--- src/app/forgotpassword/page.js
+++ src/app/forgotpassword/page.js
@@ -5,6 +5,7 @@ import { useLanguage } from "@/context/LanguageContext";
 import SEO from "@/components/SEO/SEO";
 
 import Link from "next/link";
+import TextInput from "@/components/TextInput/TextInput";
 const ForgotPassword = () => {
   const { translations } = useLanguage();
 
@@ -42,22 +43,52 @@ const ForgotPassword = () => {
           {" "}
           {translations["forgotPassword.paragraph"]}
         </p>
-        <p className="text-left">Email Address</p>
-        <input
-          type="email"
-          placeholder={translations["forgotPassword.input"]}
-          className="mb-4 mt-2 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <div className="mb-4 mt-2 w-full">
-          <Link href="/check-in-box">
-            <Button
-              text={translations["forgotPassword.button"]}
-              width="full"
-              data-testid="forgot-password-send-reset-link-button"
-              aria-label="forgot password send reset link button"
-            />{" "}
-          </Link>
-        </div>
+        <Formik
+          initialValues={{ email: "" }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            // Handle form submission
+          }}
+          validateOnMount={true}
+          validateOnChange={true}
+          validateOnBlur={true}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+            isSubmitting,
+            isValid,
+            dirty,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <p className="text-left">Email Address</p>
+              <TextInput
+                type="email"
+                name="email"
+                value={values.email}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={touched.email && errors.email}
+                placeholder={translations["forgotPassword.input"]}
+                className="mb-4 mt-2 w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <div className="mb-4 mt-2 w-full">
+                <Link href="/check-in-box">
+                  <Button
+                    text={translations["forgotPassword.button"]}
+                    width="full"
+                    data-testid="forgot-password-send-reset-link-button"
+                    aria-label="forgot password send reset link button"
+                  />{" "}
+                </Link>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>{" "}
     </div>
   );
